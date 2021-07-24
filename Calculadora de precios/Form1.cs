2210814b74d866ee96dd31a3e82e1fe53be370f0
resaj <resaj@LAP-RESAJE>
2021-07-23 19:53:13 -0500

    resuelve problema al insertar solo un "." en el campo de texto

--- Calculadora de precios/Form1.cs
+++ Calculadora de precios/Form1.cs
@@ -22,33 +22,41 @@ namespace Calculadora_de_precios
             validar.NumerosDecimales(e);
             if ((int)e.KeyChar == (int)Keys.Enter)
             {
-                decimal precio = Convert.ToDecimal(textBox1.Text);
-                decimal precio5, precio10, precio15, precio20, precio45;
-                precio5 = decimal.Multiply(precio, (decimal)1.05);
-                label11.Text = "$ "+Convert.ToString(decimal.Round(precio5, 0, MidpointRounding.AwayFromZero));
-                precio10 = decimal.Multiply(precio, (decimal)1.1);
-                label10.Text= "$ " + Convert.ToString(decimal.Round(precio10, 0, MidpointRounding.AwayFromZero));
-                precio15 = decimal.Multiply(precio, (decimal)1.15);
-                label9.Text= "$ " + Convert.ToString(decimal.Round(precio15, 0, MidpointRounding.AwayFromZero));
-                precio20 = decimal.Multiply(precio, (decimal)1.2);
-                label8.Text= "$ " + Convert.ToString(decimal.Round(precio20, 0, MidpointRounding.AwayFromZero));
-                precio45 = decimal.Multiply(precio, (decimal)1.45);
-                label7.Text= "$ " + Convert.ToString(decimal.Round(precio45, 0, MidpointRounding.AwayFromZero));
-                precio5 = decimal.Multiply(precio5, (decimal)1.16);
-                label17.Text= "$ " + Convert.ToString(decimal.Round(precio5, 0, MidpointRounding.AwayFromZero));
-                precio10 = decimal.Multiply(precio10, (decimal)1.16);
-                label16.Text = "$ " + Convert.ToString(decimal.Round(precio10, 0, MidpointRounding.AwayFromZero));
-                precio15 = decimal.Multiply(precio15, (decimal)1.16);
-                label15.Text = "$ " + Convert.ToString(decimal.Round(precio15, 0, MidpointRounding.AwayFromZero));
-                precio20 = decimal.Multiply(precio20, (decimal)1.16);
-                label14.Text = "$ " + Convert.ToString(decimal.Round(precio20, 0, MidpointRounding.AwayFromZero));
-                precio45 = decimal.Multiply(precio45, (decimal)1.16);
-                label13.Text = "$ " + Convert.ToString(decimal.Round(precio45, 0, MidpointRounding.AwayFromZero));
-                if (!String.IsNullOrEmpty(textBox1.Text))
+                if (textBox1.Text.Equals("."))
                 {
-                    textBox1.SelectionStart = 0;
-                    textBox1.SelectionLength = textBox1.Text.Length;
+                    MessageBox.Show("Solo numeros o numeros con punto decimal");
                 }
+                else
+                {
+                    decimal precio = Convert.ToDecimal(textBox1.Text);
+                    decimal precio5, precio10, precio15, precio20, precio45;
+                    precio5 = decimal.Multiply(precio, (decimal)1.05);
+                    label11.Text = "$ " + Convert.ToString(decimal.Round(precio5, 0, MidpointRounding.AwayFromZero));
+                    precio10 = decimal.Multiply(precio, (decimal)1.1);
+                    label10.Text = "$ " + Convert.ToString(decimal.Round(precio10, 0, MidpointRounding.AwayFromZero));
+                    precio15 = decimal.Multiply(precio, (decimal)1.15);
+                    label9.Text = "$ " + Convert.ToString(decimal.Round(precio15, 0, MidpointRounding.AwayFromZero));
+                    precio20 = decimal.Multiply(precio, (decimal)1.2);
+                    label8.Text = "$ " + Convert.ToString(decimal.Round(precio20, 0, MidpointRounding.AwayFromZero));
+                    precio45 = decimal.Multiply(precio, (decimal)1.45);
+                    label7.Text = "$ " + Convert.ToString(decimal.Round(precio45, 0, MidpointRounding.AwayFromZero));
+                    precio5 = decimal.Multiply(precio5, (decimal)1.16);
+                    label17.Text = "$ " + Convert.ToString(decimal.Round(precio5, 0, MidpointRounding.AwayFromZero));
+                    precio10 = decimal.Multiply(precio10, (decimal)1.16);
+                    label16.Text = "$ " + Convert.ToString(decimal.Round(precio10, 0, MidpointRounding.AwayFromZero));
+                    precio15 = decimal.Multiply(precio15, (decimal)1.16);
+                    label15.Text = "$ " + Convert.ToString(decimal.Round(precio15, 0, MidpointRounding.AwayFromZero));
+                    precio20 = decimal.Multiply(precio20, (decimal)1.16);
+                    label14.Text = "$ " + Convert.ToString(decimal.Round(precio20, 0, MidpointRounding.AwayFromZero));
+                    precio45 = decimal.Multiply(precio45, (decimal)1.16);
+                    label13.Text = "$ " + Convert.ToString(decimal.Round(precio45, 0, MidpointRounding.AwayFromZero));
+                }
+                    if (!String.IsNullOrEmpty(textBox1.Text))
+                    {
+                        textBox1.SelectionStart = 0;
+                        textBox1.SelectionLength = textBox1.Text.Length;
+                    }
+                
             }
         }
     }
