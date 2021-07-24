3623ee341aff3557fe360e6e658f42b6fcea0474
resaj <resaj@LAP-RESAJE>
2021-07-23 19:00:12 -0500

    VERSION ALFA 1

--- Calculadora de precios/Form1.Designer.cs
+++ Calculadora de precios/Form1.Designer.cs
@@ -52,7 +52,7 @@ namespace Calculadora_de_precios
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox1.Location = new System.Drawing.Point(24, 172);
+            this.textBox1.Location = new System.Drawing.Point(391, 41);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(200, 43);
             this.textBox1.TabIndex = 0;
@@ -63,7 +63,7 @@ namespace Calculadora_de_precios
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(229, 175);
+            this.label1.Location = new System.Drawing.Point(473, 131);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 37);
             this.label1.TabIndex = 1;
@@ -73,7 +73,7 @@ namespace Calculadora_de_precios
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(260, 103);
+            this.label2.Location = new System.Drawing.Point(293, 192);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(54, 37);
             this.label2.TabIndex = 2;
@@ -83,7 +83,7 @@ namespace Calculadora_de_precios
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(260, 140);
+            this.label3.Location = new System.Drawing.Point(293, 229);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(69, 37);
             this.label3.TabIndex = 3;
@@ -93,7 +93,7 @@ namespace Calculadora_de_precios
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(260, 177);
+            this.label4.Location = new System.Drawing.Point(293, 266);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(69, 37);
             this.label4.TabIndex = 4;
@@ -103,7 +103,7 @@ namespace Calculadora_de_precios
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label5.Location = new System.Drawing.Point(260, 214);
+            this.label5.Location = new System.Drawing.Point(293, 303);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(69, 37);
             this.label5.TabIndex = 5;
@@ -113,7 +113,7 @@ namespace Calculadora_de_precios
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label6.Location = new System.Drawing.Point(260, 251);
+            this.label6.Location = new System.Drawing.Point(293, 340);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(69, 37);
             this.label6.TabIndex = 6;
@@ -123,7 +123,7 @@ namespace Calculadora_de_precios
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label7.Location = new System.Drawing.Point(344, 251);
+            this.label7.Location = new System.Drawing.Point(377, 340);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(47, 37);
             this.label7.TabIndex = 11;
@@ -133,7 +133,7 @@ namespace Calculadora_de_precios
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label8.Location = new System.Drawing.Point(344, 214);
+            this.label8.Location = new System.Drawing.Point(377, 303);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(47, 37);
             this.label8.TabIndex = 10;
@@ -143,7 +143,7 @@ namespace Calculadora_de_precios
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label9.Location = new System.Drawing.Point(344, 177);
+            this.label9.Location = new System.Drawing.Point(377, 266);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(47, 37);
             this.label9.TabIndex = 9;
@@ -153,7 +153,7 @@ namespace Calculadora_de_precios
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label10.Location = new System.Drawing.Point(344, 140);
+            this.label10.Location = new System.Drawing.Point(377, 229);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(47, 37);
             this.label10.TabIndex = 8;
@@ -163,7 +163,7 @@ namespace Calculadora_de_precios
             // 
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label11.Location = new System.Drawing.Point(344, 103);
+            this.label11.Location = new System.Drawing.Point(377, 192);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(47, 37);
             this.label11.TabIndex = 7;
@@ -173,7 +173,7 @@ namespace Calculadora_de_precios
             // 
             this.label12.AutoSize = true;
             this.label12.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label12.Location = new System.Drawing.Point(452, 178);
+            this.label12.Location = new System.Drawing.Point(485, 267);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(140, 37);
             this.label12.TabIndex = 12;
@@ -183,7 +183,7 @@ namespace Calculadora_de_precios
             // 
             this.label13.AutoSize = true;
             this.label13.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label13.Location = new System.Drawing.Point(590, 251);
+            this.label13.Location = new System.Drawing.Point(623, 340);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(47, 37);
             this.label13.TabIndex = 17;
@@ -193,7 +193,7 @@ namespace Calculadora_de_precios
             // 
             this.label14.AutoSize = true;
             this.label14.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label14.Location = new System.Drawing.Point(590, 214);
+            this.label14.Location = new System.Drawing.Point(623, 303);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(47, 37);
             this.label14.TabIndex = 16;
@@ -203,7 +203,7 @@ namespace Calculadora_de_precios
             // 
             this.label15.AutoSize = true;
             this.label15.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label15.Location = new System.Drawing.Point(590, 177);
+            this.label15.Location = new System.Drawing.Point(623, 266);
             this.label15.Name = "label15";
             this.label15.Size = new System.Drawing.Size(47, 37);
             this.label15.TabIndex = 15;
@@ -213,7 +213,7 @@ namespace Calculadora_de_precios
             // 
             this.label16.AutoSize = true;
             this.label16.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label16.Location = new System.Drawing.Point(590, 140);
+            this.label16.Location = new System.Drawing.Point(623, 229);
             this.label16.Name = "label16";
             this.label16.Size = new System.Drawing.Size(47, 37);
             this.label16.TabIndex = 14;
@@ -223,7 +223,7 @@ namespace Calculadora_de_precios
             // 
             this.label17.AutoSize = true;
             this.label17.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label17.Location = new System.Drawing.Point(590, 103);
+            this.label17.Location = new System.Drawing.Point(623, 192);
             this.label17.Name = "label17";
             this.label17.Size = new System.Drawing.Size(47, 37);
             this.label17.TabIndex = 13;
