492a462b5ac4f2d3de3459c0cd01f84830806884
resaj <resaj@LAP-RESAJE>
2021-08-29 10:48:56 -0500

    cambio de color de fuente

--- Calculadora de precios/Form1.Designer.cs
+++ Calculadora de precios/Form1.Designer.cs
@@ -73,6 +73,7 @@ namespace Calculadora_de_precios
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.DarkGreen;
             this.label2.Location = new System.Drawing.Point(293, 192);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(54, 37);
@@ -83,6 +84,7 @@ namespace Calculadora_de_precios
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.Olive;
             this.label3.Location = new System.Drawing.Point(293, 229);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(69, 37);
@@ -93,6 +95,7 @@ namespace Calculadora_de_precios
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.RoyalBlue;
             this.label4.Location = new System.Drawing.Point(293, 266);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(69, 37);
@@ -113,6 +116,7 @@ namespace Calculadora_de_precios
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.Color.Crimson;
             this.label6.Location = new System.Drawing.Point(293, 340);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(69, 37);
@@ -123,6 +127,7 @@ namespace Calculadora_de_precios
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.ForeColor = System.Drawing.Color.Crimson;
             this.label7.Location = new System.Drawing.Point(377, 340);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(47, 37);
@@ -143,6 +148,7 @@ namespace Calculadora_de_precios
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label9.ForeColor = System.Drawing.Color.RoyalBlue;
             this.label9.Location = new System.Drawing.Point(377, 266);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(47, 37);
@@ -153,6 +159,7 @@ namespace Calculadora_de_precios
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label10.ForeColor = System.Drawing.Color.Olive;
             this.label10.Location = new System.Drawing.Point(377, 229);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(47, 37);
@@ -163,6 +170,7 @@ namespace Calculadora_de_precios
             // 
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label11.ForeColor = System.Drawing.Color.DarkGreen;
             this.label11.Location = new System.Drawing.Point(377, 192);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(47, 37);
@@ -183,6 +191,7 @@ namespace Calculadora_de_precios
             // 
             this.label13.AutoSize = true;
             this.label13.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label13.ForeColor = System.Drawing.Color.Crimson;
             this.label13.Location = new System.Drawing.Point(623, 340);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(47, 37);
@@ -203,6 +212,7 @@ namespace Calculadora_de_precios
             // 
             this.label15.AutoSize = true;
             this.label15.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label15.ForeColor = System.Drawing.Color.RoyalBlue;
             this.label15.Location = new System.Drawing.Point(623, 266);
             this.label15.Name = "label15";
             this.label15.Size = new System.Drawing.Size(47, 37);
@@ -213,6 +223,7 @@ namespace Calculadora_de_precios
             // 
             this.label16.AutoSize = true;
             this.label16.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label16.ForeColor = System.Drawing.Color.Olive;
             this.label16.Location = new System.Drawing.Point(623, 229);
             this.label16.Name = "label16";
             this.label16.Size = new System.Drawing.Size(47, 37);
@@ -223,6 +234,7 @@ namespace Calculadora_de_precios
             // 
             this.label17.AutoSize = true;
             this.label17.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label17.ForeColor = System.Drawing.Color.DarkGreen;
             this.label17.Location = new System.Drawing.Point(623, 192);
             this.label17.Name = "label17";
             this.label17.Size = new System.Drawing.Size(47, 37);
@@ -233,6 +245,7 @@ namespace Calculadora_de_precios
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightYellow;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label13);
             this.Controls.Add(this.label14);
